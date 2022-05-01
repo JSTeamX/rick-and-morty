cbc8a00495176a2d3a910f723a473047919e8c4a
Herman Andres <heanfig@gmail.com>
2022-05-01 09:30:28 -0500

    initial commit

---
+++ apps/rick-and-morty-dex/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'rick-and-morty-dex',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/rick-and-morty-dex',
+};
